f5a6e0f4b91ff505628149bda3e2fe5e10a8560c
seeilogiic <jrmor303@gmail.com>
2026-06-08 18:59:36 -0500

    initial assignment 3 commit

---
+++ Favorites/Favorites/FavoritesApp.swift
@@ -0,0 +1,17 @@
+//
+// FavoritesApp.swift : Favorites
+//
+// Copyright © 2025 Auburn University.
+// All Rights Reserved.
+
+
+import SwiftUI
+
+@main
+struct FavoritesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
